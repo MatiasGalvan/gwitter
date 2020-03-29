4590ed0993518f75cdfb96514b73a99e6f09dc97
Matias Galvan <matiasgalvan92@gmail.com>
2020-03-29 16:51:42 -0300

    Adding tab nav in home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { TweetComponent } from './components/tweet/tweet.component';
 import { pathToFileURL } from 'url';
 
 const routes: Routes = [
-  { path: 'nav', component: NavbarComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  
+  { path: 'navbar', component: NavbarComponent },//Son de prueba para el tab nav
+  { path: 'settings', component: TweetComponent }//---
 ];
 
 @NgModule({
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,28 +4,55 @@ import { Tweet } from '../../interfaces/IMovie';
 import { GwitterService } from '../../services/movie.service';
 */
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  //timeline: Tweet[];
+  //timeline: any[];
   tweet: String;
+  navLinks: any[];
+  activeLinkIndex = -1;
 
   constructor(
-    //private gwitterService: GwitterService
-  ) { }
+  //  private tweetService: TweetService,
+    private router: Router,
+  ) {
+    this.navLinks = [
+      {
+          label: 'Home',
+          link: '/home',
+          index: 0
+      }, {
+          label: 'Search',
+          link: '/navbar',
+          index: 1
+      }, {
+          label: 'Settings',
+          link: '/settings',
+          index: 2
+      }, 
+    ];
+  }
 
   ngOnInit(): void {
-    //this.getTimeline();
-    this.tweet="Lorem ipsum dolor"
+
+    this.router.events.subscribe((res) => {
+      this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.link === '.' + this.router.url));
+    });
+
+    /*this.getTimeline();
+    console.log(this.timeline);*/
+    this.tweet="Lorem ipsum dolor";
   }
 
-  /* 
-  getTimeline(): void {
-    this.gwitterService.getMovies()
-        .subscribe(movies => this.timeline= movies);
-  }
-  */
+   
+  /*getTimeline(): void {
+    this.tweetService.getTimeline()
+      .subscribe(timeline => this.timeline = timeline);
+  }*/
+
 }
