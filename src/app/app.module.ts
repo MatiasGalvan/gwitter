2bbda82bba59c6ae8030a307367be89e62dcb230
Luciano_Sassano <lucianosassano1@gmail.com>
2020-03-30 20:00:27 -0300

    last commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { TweetComponent } from './components/tweet/tweet.component';
+import { TweetServiceClient } from './services/TweetServiceClient'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    TweetComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +29,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     MatButtonModule
 
   ],
-  providers: [],
+  providers: [TweetServiceClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
