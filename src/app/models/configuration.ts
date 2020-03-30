661f05a02ebb9728e516c0f3bb401c4321b6e154
Antonella <antonellabuono4@gmail.com>
2020-03-30 00:03:59 -0300

    Agregue models con sus respectivas interfaces y cree tweet-details

---
+++ src/app/models/configuration.ts
@@ -0,0 +1,7 @@
+export interface IConfiguration {
+  hushNotVerifiedAccount : boolean;
+  hushNotFollowed : boolean;
+  hushDefaultProfile : boolean;
+  hushTweetsWithLink : boolean;
+  hushTextTruncated : boolean;
+}
---
+++ src/app/models/trends.ts
@@ -0,0 +1,8 @@
+export interface ITrends {
+  id : number;
+  name : string;
+  url : string;
+  prometed_content : any;
+  query : string;
+  tweet_volume : number; 
+}
