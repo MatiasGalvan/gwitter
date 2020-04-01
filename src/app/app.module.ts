fe1c830f97ada9ecb06b30b23c7bd55194d76b97
Luciano_Sassano <lucianosassano1@gmail.com>
2020-04-01 01:23:33 -0300

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,41 @@
+/*------------------------Material Angular Components------------------------ */
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { TrendService } from 'src/app/services/trend.service';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TrendListComponent } from './components/trend-list/trend-list.component';
-import { FormsModule } from '@angular/forms';
-import { TrendpipePipe } from './pipes/trendpipe.pipe';
 import { MatListModule } from '@angular/material/list';
 import { MatInputModule } from '@angular/material/input';
+
+/*------------------------Angular Modules-------------------------------------*/
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+
+/*------------------------Custom Components---------------------------------- */
 import { TweetComponent } from './components/tweet/tweet.component';
 import { NavtabComponent } from './components/navtab/navtab.component';
 import { TweetDetailsComponent } from './components/tweet-details/tweet-details.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { HomeComponent } from './components/home/home.component';
+import { TrendListComponent } from './components/trend-list/trend-list.component';
+
+/*----------------------------Services----------------------------------------*/
+import { TrendService } from './services/trend.service'
+
+/*-----------------------------Others-----------------------------------------*/
+import { AppComponent } from './app.component';
+import { TrendpipePipe } from './pipes/trendpipe.pipe';
+
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -31,6 +46,7 @@ import { TweetDetailsComponent } from './components/tweet-details/tweet-details.
     TweetComponent,
     NavtabComponent,
     TweetDetailsComponent,
+    SettingsComponent,
   ],
   imports: [
     BrowserModule,
